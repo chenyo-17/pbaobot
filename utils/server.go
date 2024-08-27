146decace9be946fbd6f43dec5bc0b768cff13f1
chenyo-17 <cy970802@hotmail.com>
2024-08-27 22:40:48 +0200

    fix: remove db debug from the server

--- utils/server.go
+++ utils/server.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/dgraph-io/badger/v4"
 )
@@ -14,51 +13,14 @@ func StartHTTPServer(db *badger.DB, logger *BotLogger, port string) {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Bot is running!"))
 	})
-	// health check engpoint, return 200 OK
+	// health check endpoint, return 200 OK
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, "OK")
 	})
 
-	// for database debugging
-	http.HandleFunc("/debug", func(w http.ResponseWriter, r *http.Request) {
-		// add basic auth
-		user, pass, ok := r.BasicAuth()
-		if !ok || user != os.Getenv("DB_USER") || pass != os.Getenv("DB_PASS") {
-			w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
-			http.Error(w, "Unauthorized", http.StatusUnauthorized)
-			return
-		}
-
-		err := db.View(func(txn *badger.Txn) error {
-			opts := badger.DefaultIteratorOptions
-			it := txn.NewIterator(opts)
-			defer it.Close()
-
-			fmt.Fprintf(w, "<h1>Database Contents</h1>")
-			for it.Rewind(); it.Valid(); it.Next() {
-				item := it.Item()
-				k := item.Key()
-				err := item.Value(func(v []byte) error {
-					fmt.Fprintf(w, "<p>Key: %s, Value: %s</p>", k, v)
-					return nil
-				})
-				if err != nil {
-					return err
-				}
-			}
-			return nil
-		})
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	})
-
-	fmt.Println("Debug server starting on :" + port)
-	logger.Println("Debug server starting on :" + port)
-	// logger.Fatal(http.ListenAndServe(":"+port, nil))
 	addr := fmt.Sprintf("0.0.0.0:%s", port)
+	logger.Printf("HTTP server starting on %s", addr)
 	server := &http.Server{
 		Addr: addr,
 	}
